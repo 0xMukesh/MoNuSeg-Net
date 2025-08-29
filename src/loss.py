d5b3e41c2da736fcba76a83237fd0182f2ae5372
0xMukesh <hey.mukesh272921@gmail.com>
2025-08-29 23:23:56 +0530

    fix: fix shape mismatch issue with multi class dice score

--- src/loss.py
+++ src/loss.py
@@ -3,33 +3,36 @@ from torch import nn
 import torch.nn.functional as F
 from torchmetrics.segmentation.dice import DiceScore
 from einops import rearrange
+from typing import Literal
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha: float = 0.9, gamma: float = 2) -> None:
+    def __init__(self, alpha: float = 0.2, gamma: float = 2) -> None:
         super().__init__()
 
         self.alpha = alpha
         self.gamma = gamma
 
-    def forward(self, preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        if preds.ndim > 2:
-            preds = rearrange(preds, "b c h w -> (b h w) c")
-            targets = rearrange(targets, "b c h w -> (b h w) c")
+    def forward(
+        self,
+        preds: torch.Tensor,
+        targets: torch.Tensor,
+        reduction: Literal["mean", "sum"] = "mean",
+    ) -> torch.Tensor:
+        targets = rearrange(targets, "b 1 h w -> b h w").type(torch.long)
+        ce_loss = F.cross_entropy(input=preds, target=targets, reduction="none")
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
 
-        logpt = F.log_softmax(preds, dim=1)
-        pt = logpt.exp()
-
-        logpt = logpt.gather(dim=1, index=targets.type(torch.int32)).squeeze(1)
-        pt = pt.gather(dim=1, index=targets.type(torch.int64)).squeeze(1)
-
-        loss = -self.alpha * (1 - pt) ** self.gamma * logpt
-        return loss.mean()
+        if reduction == "mean":
+            return focal_loss.mean()
+        else:
+            return focal_loss.sum()
 
 
 class FocalDiceLoss(nn.Module):
     def __init__(
-        self, num_classes: int, alpha: float = 0.9, beta: float = 0.5, gamma: float = 2
+        self, num_classes: int, alpha: float = 0.25, beta: float = 0.5, gamma: float = 2
     ) -> None:
         super().__init__()
 
@@ -38,10 +41,26 @@ class FocalDiceLoss(nn.Module):
         self.gamma = gamma
         self.num_classes = num_classes
 
-        self.dice = DiceScore(num_classes, average="micro")
+        self.dice = DiceScore(num_classes, average="macro", input_format="index")
         self.focal = FocalLoss(alpha, gamma)
 
     def forward(self, preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-        return (1 - self.beta) * self.focal(preds, targets) + self.beta * self.dice(
-            preds, targets
-        )
+        focal_loss = self.focal(preds, targets)
+
+        targets = targets.squeeze(1).long()
+        targets = F.one_hot(targets, num_classes=5)
+        targets = targets.permute(0, 3, 1, 2)
+
+        preds = torch.softmax(preds, dim=1).long()
+        dice_score = self.dice(preds, targets)
+        dice_loss = 1 - dice_score
+
+        return (1 - self.beta) * focal_loss + self.beta * dice_loss
+
+
+if __name__ == "__main__":
+    preds = torch.floor(torch.randn(8, 5, 256, 256) * 5)
+    targets = torch.randint(0, 5, (8, 1, 256, 256)).type(torch.float)
+    criterion = FocalDiceLoss(num_classes=5)
+    loss = criterion(preds, targets)
+    print(loss)
